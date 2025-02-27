17cdc805b60f99fad3e11758c289cbdf7bcb6e07
MajoRC221051 <110446328+MajoRC221051@users.noreply.github.com>
2025-02-27 16:57:08 -0600

    Update AbstractStack.java

--- Code/AbstractStack.java
+++ Code/AbstractStack.java
@@ -1,3 +1,7 @@
+/**
+ * The `AbstractStack` class is an abstract class that implements the `Stack` interface and provides a
+ * method to check if the stack is empty.
+ */
 import java.util.List;
 
 abstract class AbstractStack<T> implements Stack<T> {
