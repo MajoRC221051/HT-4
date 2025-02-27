691a6b9f96fc9bfe59bfd85655902eb664ac9d1f
MajoRC221051 <110446328+MajoRC221051@users.noreply.github.com>
2025-02-27 16:55:49 -0600

    Update Stack.java

--- Code/Stack.java
+++ Code/Stack.java
@@ -1,6 +1,12 @@
+// This code defines an interface named `Stack` with a generic type `T`. It declares four methods that
+// a class implementing this interface must provide:
+
 interface Stack<T> {
     void push(T item);
+
     T pop();
+
     T peek();
+
     boolean isEmpty();
 }
