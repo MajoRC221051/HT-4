545c52f4ba3c8e7e385761eb4a71528a7eb2b250
MajoRC221051 <110446328+MajoRC221051@users.noreply.github.com>
2025-02-27 16:58:14 -0600

    Update StackTests.java

--- Test/StackTests.java
+++ Test/StackTests.java
@@ -1,3 +1,7 @@
+/**
+ * The class StackTests contains JUnit tests for ArrayListStack and VectorStack implementations of a
+ * stack data structure in Java.
+ */
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
@@ -11,4 +15,49 @@ class StackTests {
         assertEquals(10, stack.peek());
         assertEquals(10, stack.pop());
         assertTrue(stack.isEmpty());
-    }}
+    }
+
+    @Test
+    void testVectorStack() {
+        Stack<Integer> stack = new VectorStack<>();
+        stack.push(20);
+        assertEquals(20, stack.peek());
+        assertEquals(20, stack.pop());
+        assertTrue(stack.isEmpty());
+    }
+
+    
+    /** 
+     * @return Object
+     * @throws CloneNotSupportedException
+     */
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        // TODO Auto-generated method stub
+        return super.clone();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return super.equals(obj);
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        // TODO Auto-generated method stub
+        super.finalize();
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+}
