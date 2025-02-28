9dc77d0b5a68a95a60d09a167087c5bc4eb79ac3
Pjezz <p.jezraelhuerta@gmail.com>
2025-02-27 23:25:56 -0600

    Corrigiendo error
    Corrigiendo error por prueba unitaria

--- Code/ListStack.java
+++ Code/ListStack.java
@@ -1,12 +1,11 @@
 class ListStack<T> {
     private SinglyLinkedList<T> list = new SinglyLinkedList<>();
 
-    /**
-     * @param data
-     */
-
     public void push(T data) {
         list.add(data);
     }
 
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
 }
